5c80a0a02f14cbc5f83c294fe487a0cb64148a78
maidiyangsa <57142580+maidiyangsa@users.noreply.github.com>
2020-10-18 14:15:41 +0800

    integer

---
+++ Integer.java
@@ -0,0 +1,37 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package integer;
+
+/**
+ *
+ * @author Beta Three
+ * NAMA     : ADRIAN MAIDIYANGSA
+ * KELAS    : IF-1
+ * NIM      : 10119029
+ */
+public class Integer {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        
+        byte b;
+        short s;
+        int i;
+        long l;
+        b = 120;
+        s = 32767;
+        i = 1_000_000_000;
+        l = 90000000000L;
+        System.out.println("byte : " + b);
+        System.out.println("short : " + s);
+        System.out.println("int : " + i);
+        System.out.println("long : " + l);
+    }
+    
+}
